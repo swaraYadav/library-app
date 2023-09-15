77b5f8955dc3f49b0eebf38e6e5dbe51b8aca3bf
swaraYadav <enggswarnlatayadav@gmail.com>
2023-09-15 12:50:38 +0530

    new test changes

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -59,7 +59,7 @@ function Homepage(){
                                    
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-primary">Create new account</button>
+                                        <button type="button" class="btn btn-primary">Create account</button>
                                     </div>
                                     </div>
                                 </div>
